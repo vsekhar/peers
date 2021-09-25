27b4de12b61a3308bdd4eb4af99071988e3459f7
Vivek Sekhar <vivek@viveksekhar.ca>
2021-09-25 19:17:30 -0400

    Explain singlego.

--- internal/singlego/singlego.go
+++ internal/singlego/singlego.go
@@ -1,5 +1,11 @@
 package singlego
 
+// Package singlego is similar to golang.org/x/sync/singleflight except we skip
+// the work to obtain and pass on return values. This is more suited to local
+// function calls (hence "go" in singlego) where the closure will handle routing
+// results and/or logging errors, as opposed to remote API calls which is the
+// target of singleflight.
+
 import "context"
 
 type Trigger chan struct{}
