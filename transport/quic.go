7c6497813f1189b1253adc3400f26a834fa72ce5
Vivek Sekhar <vivek@viveksekhar.ca>
2022-06-01 00:01:21 -0400

    s/Session/Connection

--- transport/quic.go
+++ transport/quic.go
@@ -29,6 +29,7 @@ type quicMessage struct {
 	addr net.Addr
 }
 
+// quicConn emulates a net.Conn using a quic.Stream.
 type quicConn struct {
 	quic.Stream
 	conn quic.Connection
@@ -40,29 +41,29 @@ func (q *quicConn) Close() error         { return q.Stream.Close() }
 func (q *quicConn) LocalAddr() net.Addr  { return q.conn.LocalAddr() }
 func (q *quicConn) RemoteAddr() net.Addr { return q.conn.RemoteAddr() }
 
-type quicSession2 struct {
+type quicConnection struct {
 	ctx    context.Context
 	cancel func()
 	qConn  quic.Connection
 }
 
-func newQUICSession2(ctx context.Context, session quic.Connection, streams chan<- *quicConn, messages chan<- *quicMessage) *quicSession2 {
+func newQUICConnection(ctx context.Context, conn quic.Connection, streams chan<- *quicConn, messages chan<- *quicMessage) *quicConnection {
 	sessCtx, cancel := context.WithCancel(ctx)
 
 	// Stream pump
 	go func() {
 		for {
-			stream, err := session.AcceptStream(sessCtx)
+			stream, err := conn.AcceptStream(sessCtx)
 			if errors.Is(err, context.Canceled) {
 				return
 			}
 			if err != nil {
-				session.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), err.Error())
+				conn.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), err.Error())
 				return
 			}
 			go func(stream quic.Stream) {
 				select {
-				case streams <- &quicConn{stream, session}:
+				case streams <- &quicConn{stream, conn}:
 				case <-time.After(quicTimeout):
 					stream.Close()
 				case <-sessCtx.Done():
@@ -75,12 +76,12 @@ func newQUICSession2(ctx context.Context, session quic.Connection, streams chan<
 	// Message pump
 	go func() {
 		for {
-			msg, err := session.ReceiveMessage()
+			msg, err := conn.ReceiveMessage()
 			if errors.Is(err, context.Canceled) {
 				break
 			}
 			if err != nil {
-				session.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), err.Error())
+				conn.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), err.Error())
 				return
 			}
 			go func(qm *quicMessage) {
@@ -88,40 +89,40 @@ func newQUICSession2(ctx context.Context, session quic.Connection, streams chan<
 				case messages <- qm:
 				case <-sessCtx.Done():
 				}
-			}(&quicMessage{msg, session.RemoteAddr()})
+			}(&quicMessage{msg, conn.RemoteAddr()})
 		}
 	}()
 
-	return &quicSession2{sessCtx, cancel, session}
+	return &quicConnection{sessCtx, cancel, conn}
 }
 
-func (qs *quicSession2) Close() error {
+func (qs *quicConnection) Close() error {
 	qs.cancel()
 	return nil
 }
 
-// QUIC contains two layers of abstraction (session and stream) whereas other
+// QUIC contains two layers of abstraction (connection and stream) whereas other
 // network protocols contain only one (socket). To handle this, we cache
-// QUIC sessions and re-use them when dialing or listening for new streams, and
+// QUIC connections and re-use them when dialing or listening for new streams, and
 // we pass streams on to the application to be used as traditional sockets.
 // Since we cannot trust network addresses due to NAT, each host creates a GUID
-// and sends it to the other on the first stream when creating a session. If
-// a new incoming session arrives for a given peer GUID, hosts must terminate
-// the old session.
+// and sends it to the other on the first stream when creating a connection. If
+// a new incoming connection arrives for a given peer GUID, hosts must terminate
+// the old connection.
 
-// quicSessionManager manages underlying QUIC sessions (connections) between hosts.
+// quicConnectionManager manages underlying QUIC connections between hosts.
 //
-// Sessions may be created in the outbound or inbound direction and are matched
+// Connections may be created in the outbound or inbound direction and are matched
 // using a GUID generated for each host.
 //
-// When an outbound session to a host is requested, the GUID cache is checked
+// When an outbound connection to a host is requested, the GUID cache is checked
 // to see if there is a known GUID for that host. If found, the GUID is used to
-// check if there is an active inbound or outbound session for that host. If
-// found, that active session is used. If the GUID is not found, the host is
+// check if there is an active inbound or outbound connection for that host. If
+// found, that active connection is used. If the GUID is not found, the host is
 // dialed, its GUID obtained and cached.
 //
-// quicSessionManager listens for inbound sessions via the provided transport. When
-// a new session arrives, the GUID of the originating host is read and checked
+// quicSessionManager listens for inbound connections via the provided transport. When
+// a new connection arrives, the GUID of the originating host is read and checked
 //
 // quicSessionManager does not itself implement Interface, but is used by
 // quicTransport to implement Interface.
@@ -132,26 +133,26 @@ type quicSessionManager struct {
 	cancel    func()
 	tlsConfig *tls.Config
 
-	cmu      *sync.Mutex
-	hostIDs  map[string][16]byte        // network:address --> peer hostID
-	sessions map[[16]byte]*quicSession2 // peer hostID --> quicSession
-	streams  chan *quicConn
-	messages chan *quicMessage
+	cmu         *sync.Mutex
+	hostIDs     map[string][16]byte          // network:address --> peer hostID
+	connections map[[16]byte]*quicConnection // peer hostID --> quicConnection
+	streams     chan *quicConn
+	messages    chan *quicMessage
 }
 
 func QUIC(transport Interface, tlsConfig *tls.Config) Interface {
 	ctx, cancel := context.WithCancel(context.Background())
 	r := &quicSessionManager{
-		Interface: transport,
-		hostID:    uuid.New(),
-		ctx:       ctx,
-		cancel:    cancel,
-		tlsConfig: tlsConfig,
-		cmu:       new(sync.Mutex),
-		hostIDs:   make(map[string][16]byte),
-		sessions:  make(map[[16]byte]*quicSession2),
-		streams:   make(chan *quicConn),
-		messages:  make(chan *quicMessage),
+		Interface:   transport,
+		hostID:      uuid.New(),
+		ctx:         ctx,
+		cancel:      cancel,
+		tlsConfig:   tlsConfig,
+		cmu:         new(sync.Mutex),
+		hostIDs:     make(map[string][16]byte),
+		connections: make(map[[16]byte]*quicConnection),
+		streams:     make(chan *quicConn),
+		messages:    make(chan *quicMessage),
 	}
 
 	// Session pump
@@ -179,21 +180,21 @@ func QUIC(transport Interface, tlsConfig *tls.Config) Interface {
 					return
 				}
 
-				session := newQUICSession2(ctx, c, r.streams, r.messages) // start after sendID & recvID
+				connection := newQUICConnection(ctx, c, r.streams, r.messages) // start after sendID & recvID
 				r.cmu.Lock()
 				if oldSessionHostID, ok := r.hostIDs[c.RemoteAddr().String()]; ok {
 					if bytes.Equal(oldSessionHostID[:], peerHostID[:]) {
-						if oldSession, ok := r.sessions[oldSessionHostID]; ok {
+						if oldSession, ok := r.connections[oldSessionHostID]; ok {
 							if err := oldSession.Close(); err != nil {
-								log.Printf("closing old session: %s", err)
+								log.Printf("closing old connection: %s", err)
 							}
 						} else {
-							panic("transport/quic: hostIDs and sessions out of sync")
+							panic("transport/quic: hostIDs and connections out of sync")
 						}
 					}
 				}
 				r.hostIDs[c.RemoteAddr().String()] = peerHostID
-				r.sessions[peerHostID] = session
+				r.connections[peerHostID] = connection
 				r.cmu.Unlock()
 			}(s)
 
@@ -203,45 +204,45 @@ func QUIC(transport Interface, tlsConfig *tls.Config) Interface {
 	return r
 }
 
-func (q *quicSessionManager) getSession(ctx context.Context, _, address string) (*quicSession2, error) {
-	// If we've dialed this host before, reuse the session
-	var sess *quicSession2
+func (q *quicSessionManager) getSession(ctx context.Context, _, address string) (*quicConnection, error) {
+	// If we've dialed this host before, reuse the connection
+	var sess *quicConnection
 	ok := false
 	q.cmu.Lock()
 	hostID, ok := q.hostIDs[address]
 	if ok {
-		sess, ok = q.sessions[hostID]
+		sess, ok = q.connections[hostID]
 		if !ok {
-			panic("transport/quic: hostIDs and sessions out of sync")
+			panic("transport/quic: hostIDs and connections out of sync")
 		}
 		q.cmu.Unlock()
 		return sess, nil
 	}
 	q.cmu.Unlock()
 
-	// Create a new session
+	// Create a new connection
 	addr, err := net.ResolveUDPAddr("udp", address)
 	if err != nil {
 		return nil, err
 	}
-	session, err := quic.DialContext(ctx, q.Interface, addr, "", q.tlsConfig, defaultQUICConfig)
+	connection, err := quic.DialContext(ctx, q.Interface, addr, "", q.tlsConfig, defaultQUICConfig)
 	if err != nil {
 		return nil, err
 	}
 	// Send then receive IDs (opposite order from newQUICSessionManager)
-	if err := sendID(ctx, session, q.hostID); err != nil {
-		session.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), "")
+	if err := sendID(ctx, connection, q.hostID); err != nil {
+		connection.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), "")
 		return nil, err
 	}
-	peerHostID, err := recvID(ctx, session)
+	peerHostID, err := recvID(ctx, connection)
 	if err != nil {
-		session.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), "")
+		connection.CloseWithError(quic.ApplicationErrorCode(quic.InternalError), "")
 		return nil, err
 	}
 	q.cmu.Lock()
-	sess = newQUICSession2(q.ctx, session, q.streams, q.messages)
+	sess = newQUICConnection(q.ctx, connection, q.streams, q.messages)
 	q.hostIDs[address] = peerHostID
-	q.sessions[peerHostID] = sess
+	q.connections[peerHostID] = sess
 	q.cmu.Unlock()
 	return sess, nil
 }
@@ -298,7 +299,7 @@ func (q *quicSessionManager) Close() error {
 	return nil
 }
 
-// TODO: use singleflight to create sessions to a host once
+// TODO: use singleflight to create connections to a host once
 
 func sendID(ctx context.Context, c quic.Connection, hostID uuid.UUID) error {
 	stream, err := c.OpenUniStream()
